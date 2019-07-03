9d665795a33a999f16e5225eabfd0076f8b14336
NataliaColome <nmcolome@gmail.com>
2019-07-03 15:59:57 -0600

    Delete transform module

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,8 +1,4 @@
-require './lib/transform'
-
 class Enigma
-  include Transform
-
   def encrypt(message, key = key_generator, date = date_generator)
     shifts = get_shifts(key, date)
     {
@@ -29,5 +25,42 @@ class Enigma
 
   def date_generator
     Date.today.strftime('%d%m%y')
+  end
+
+  def character_set
+    ('a'..'z').to_a << ' '
+  end
+
+  def get_keys(key)
+    (0..3).to_a.map { |i| key[i..i + 1] }
+  end
+
+  def get_offsets(date)
+    square_date = date.to_i**2
+    square_date.to_s[-4..-1].split('')
+  end
+
+  def get_shifts(key, date)
+    offsets = get_offsets(date)
+    keys = get_keys(key)
+    shifts = (0..3).to_a.map { |i| offsets[i].to_i + keys[i].to_i }
+    shifts.map { |shift| shift % 27 }
+  end
+
+  def transform_msg(text, shifts, type)
+    text.split('').map do |e|
+      start = character_set.index(e)
+      transform = letter_rotation(start, shifts[0], type)
+      shifts.rotate!
+      transform
+    end
+  end
+
+  def letter_rotation(start, shift, type)
+    if type == 'code'
+      character_set.rotate(start + shift)[0]
+    else
+      character_set.rotate(start - shift)[0]
+    end
   end
 end
