783f67d3b64d3c9d49cfa844b2e3d658b0100789
NataliaColome <nmcolome@gmail.com>
2019-07-03 14:12:22 -0600

    Add Encrypt.rb cli funcionality

---
+++ lib/encrypt.rb
@@ -0,0 +1,14 @@
+require './lib/enigma.rb'
+require 'date'
+
+source = File.open(ARGV[0], 'r')
+message = source.read
+source.close
+
+encryptor = Enigma.new.encrypt(message)
+
+coded_msg = File.open(ARGV[1], 'w')
+coded_msg.write(encryptor[:encryption])
+coded_msg.close
+
+puts "Created '#{ARGV[1]}' with the key #{encryptor[:key]} and date #{encryptor[:date]}"
