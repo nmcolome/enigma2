fa56c058aabb30539db98ec48e14f1fa60f0cf2f
NataliaColome <nmcolome@gmail.com>
2019-08-15 10:24:54 -0600

    Move related tests to cipher_spec

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -11,12 +11,6 @@ RSpec.describe 'Enigma' do
     expect(@enigma).to be_an_instance_of Enigma
   end
 
-  it '#character_set' do
-    result = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
-
-    expect(@enigma.character_set).to eq result
-  end
-
   it '#key_generator' do
     expect(@enigma.key_generator).to be_an_instance_of String
     expect(@enigma.key_generator.length).to eq 5
@@ -66,26 +60,6 @@ RSpec.describe 'Enigma' do
         expect(@enigma.encrypt('hello world')).to eq result
       end
     end
-  end
-
-  it '#get_keys' do
-    expect(@enigma.get_keys('02715')).to eq %w[02 27 71 15]
-  end
-
-  it '#get_offsets' do
-    expect(@enigma.get_offsets('040895')).to eq %w[1 0 2 5]
-  end
-
-  it '#get_shifts' do
-    expect(@enigma.get_shifts('02715', '040895')).to eq [3, 0, 19, 20]
-  end
-
-  it '#transform_msg' do
-    msg = 'hello world'
-    shifts = [3, 0, 19, 20]
-    result = 'keder ohulw'.split('')
-
-    expect(@enigma.transform_msg(msg, shifts, 'code')).to eq result
   end
 
   describe '#decrypt' do
